1ca697641e3034acdd71735fe1a5b1bd3d715c64
David Kriz <davkri22@bergen.org>
2021-05-12 10:11:40 -0400

    Terminal and comments

--- src/LambdaLab.java
+++ src/LambdaLab.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class LambdaLab {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner in = new Scanner(System.in);
+
+        System.out.print(">");
+        String input = in.nextLine();
+
+        while(!input.equals("exit")){
+            System.out.print(">");
+            input = in.nextLine();
+            if (input.contains(";")){
+                input = input.substring(0, input.indexOf(";"));
+            }
+        }
+        System.out.println("Goodbye!");
     }
 }
